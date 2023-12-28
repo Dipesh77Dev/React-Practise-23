880bb5704ef48bf81e6e46760e59864e5eb3d936
28vedant <28vedantthakur@gmail.com>
2023-12-28 20:23:50 +0530

    challenge1

---
+++ Vedant/firstreact/src/components/challenges/challenge1.js
@@ -0,0 +1,27 @@
+
+
+function Challenge1() {
+    return (
+        <>
+        <h1 >Top 10 Netflix webseries</h1>
+        <ol >
+            <li>kalapani</li>
+            <li>Farzi</li>
+                <li>The Night Managers</li>
+                <li>Rana Naidu</li>
+                <li>Jubliee</li>
+                <li>Asur: Welcome to Your Dark Side</li>
+                <li>Dahaad</li>
+                <li>Saas, Bahu Aur Flamingo</li>
+                <li>Taaza Khabar</li>
+                <li>Rocket Boys</li>
+
+        </ol>
+        
+        </>
+    )
+  
+    
+  }
+  
+  export default  Challenge1
