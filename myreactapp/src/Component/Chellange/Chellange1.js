51e2bd58227319a55505881230035d6ddcb30cbe
metarun97 <metarun97@gmail.com>
2024-05-12 13:27:43 +0530

    Chellane 1 Done

---
+++ myreactapp/src/Component/Chellange/Chellange1.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Chellange1 = () => {
+    return (
+        <>
+            <h1>Top 5 Netflix Web Series</h1>
+            <p>Baby Reindeer</p>
+            <p>The Night Agent</p>
+            <p>Goodbye Earth</p>
+            <p>Dead Boy Detectives</p>
+            <p>The Gentlemen</p>
+        </>
+    )
+}
+
+export default Chellange1;
---
+++ myreactapp/src/Component/Chellange/Mainchellange.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Chellange1 from './Chellange1';
+
+const Mainchellange = () => {
+    return (
+       <Chellange1 />
+    )
+}
+
+export default Mainchellange;
